68a134616c4c21831365a3a30391c06e9ce9d417
Ndyaakn <ndyaakn@gmail.com>
2022-01-21 13:50:58 +0700

    search bar
    responsif

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -10,8 +10,50 @@
   .btn{
     background-color: #315343;
     color: white;
+
   }
+  .search-container {
+  float: right;
+  }
+
+  .search-container button {
+  float: right;
+  padding: 6px 10px;
+  margin-top: 8px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  cursor: pointer;
+}
+.search-container button:hover {
+  background: #ccc;
+}
+
+  input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border-left: solid 1px #315343;
+  border-top: solid 1px #315343;
+  border-bottom: solid 1px #315343;
+  border-right: none;
+  border-radius: 10px 0px 0px 10px
+  
+}
+.btn_search{
+  height: 35px;
+  border-right: solid 1px #315343;
+  border-top: solid 1px #315343;
+  border-bottom:solid 1px #315343;
+  border-left:none;
+  border-radius: 0px 10px 10px 0px
+}
+
   @media (max-width: 767.98px) { 
+    .search-container {
+      float: left;
+      margin: 0px 20px
+      }
     .table {
       display: block;
       overflow: scroll;
@@ -26,19 +68,33 @@
     }
     .navbar-nav{
       margin-top: 0px !important
-    
+    }
   }
   .btn {
     margin:20px
   }
    }
 </style>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
 <div class="card">
   <div class="card-body">
-    <a href="{{ route('product.create') }}" class="btn">
-      ADD DATA
-    </a>
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <a href="{{ route('product.create') }}" class="btn">
+        ADD DATA
+        </a>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="search-container">
+          <form action="/action_page.php">
+            <input type="text" placeholder="Search.." name="search">
+            <button type="submit" class="btn_search"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
+      </div>
+    </div>
+    
     <table class="table table-striped">
       <thead>
         <tr>
